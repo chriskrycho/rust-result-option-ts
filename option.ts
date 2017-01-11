78b385f307d50c91628bc7df8944183f17630dab
Chris Krycho <chris@chriskrycho.com>
2017-01-10 19:41:45 -0500

    Improve readability heaps and gobs.

--- option.ts
+++ option.ts
@@ -24,6 +24,7 @@ interface IOption<T> {
 class OptionError extends Error {
   constructor(reason: string) {
     super()
+    this.name = 'OptionError'
     this.message = reason
   }
 }
@@ -32,44 +33,136 @@ export class Some<T> implements IOption<T> {
   private value: T
   constructor(t: T) { this.value = t }
 
-  and<U>(u: U): Option<U> { return new Some(u) }
-  andThen<U, Fn extends (t: T) => Option<U>>(fn: Fn): Option<U> { return fn(this.value) }
-  expect(msg: string): T { return this.value }
-  isNone(): boolean { return false }
-  isSome(): boolean { return true }
-  map<U, Fn extends (t: T) => U>(fn: Fn): Option<U> { return new Some(fn(this.value)) }
-  mapOr<U, Fn extends (t: T) => U>(def: U, fn: Fn): U { return fn(this.value) }
-  mapOrElse<U, D extends () => U, Fn extends (t: T) => U>(def: D, fn: Fn): U { return fn(this.value) }
-  okOr<E>(err: E): Result<T, E> { return new Ok(this.value) }
-  okOrElse<E, Fn extends () => E>(fn: Fn): Result<T, E> { return new Ok(this.value) }
-  or(orT: Option<T>): Option<T> { return this }
-  orElse<Fn extends () => Option<T>>(fn: Fn): Option<T> { return this }
+  and<U>(u: U): Option<U> { 
+    return new Some(u)
+  }
+
+  andThen<U, Fn extends (t: T) => Option<U>>(fn: Fn): Option<U> { 
+    return fn(this.value)
+  }
+
+  expect(msg: string): T {
+    return this.value
+  }
+
+  isNone(): boolean {
+    return false
+  }
+
+  isSome(): boolean {
+    return true
+  }
+
+  map<U, Fn extends (t: T) => U>(fn: Fn): Option<U> {
+    return new Some(fn(this.value))
+  }
+
+  mapOr<U, Fn extends (t: T) => U>(def: U, fn: Fn): U {
+    return fn(this.value)
+  }
+
+  mapOrElse<U, D extends () => U, Fn extends (t: T) => U>(def: D, fn: Fn): U {
+    return fn(this.value)
+  }
+
+  okOr<E>(err: E): Result<T, E> {
+    return new Ok(this.value)
+  }
+
+  okOrElse<E, Fn extends () => E>(fn: Fn): Result<T, E> {
+    return new Ok(this.value)
+  }
+
+  or(orT: Option<T>): Option<T> {
+    return this
+  }
+
+  orElse<Fn extends () => Option<T>>(fn: Fn): Option<T> {
+    return this
+  }
+
   take(): Option<T> {
     const value = this.value
     delete this.value
     Object.setPrototypeOf(this, new None())
     return new Some(value)
   }
-  unwrap(): T { return this.value }
-  unwrapOr(def: T): T { return this.value }
-  unwrapOrElse<Fn extends () => T>(fn: Fn): T { return this.value }
+
+  unwrap(): T {
+    return this.value
+  }
+
+  unwrapOr(def: T): T {
+    return this.value
+  }
+
+  unwrapOrElse<Fn extends () => T>(fn: Fn): T {
+    return this.value
+  }
 }
 
 export class None implements IOption<any> {
-  isSome(): boolean { return false }
-  isNone(): boolean { return true }
-  expect(msg: string): never { throw new OptionError(msg) }
-  unwrap(): never { throw new OptionError('Tried to `unwrap` a `None`') }
-  unwrapOr<T>(def: T): T { return def }
-  unwrapOrElse<T, Fn extends () => T>(fn: Fn): T { return fn() }
-  map<T, U, Fn extends (t: T) => U>(fn: Fn): Option<U> { return this }
-  mapOr<T, U, Fn extends (t: T) => U>(def: U, fn: Fn): U { return def }
-  mapOrElse<T, U, D extends () => U, Fn extends (t: T) => U>(def: D, fn: Fn): U { return def() }
-  okOr<E>(err: E): Result<any, E> { return new Err(err) }
-  okOrElse<E, Fn extends () => E>(fn: Fn): Result<any, E> { return new Err(fn()) }
-  and<U>(andU: U): Option<U> { return this }
-  andThen<T, U, Fn extends (t: T) => Option<U>>(fn: Fn): Option<U> { return this }
-  or<T>(t: Option<T>): Option<T> { return t }
-  orElse<T, Fn extends () => Option<T>>(fn: Fn): Option<T> { return fn() }
-  take<T>(): Option<T> { return this }
+  isSome(): boolean {
+    return false
+  }
+
+  isNone(): boolean {
+    return true
+  }
+
+  expect(msg: string): never {
+    throw new OptionError(msg)
+  }
+  
+  unwrap(): never {
+    throw new OptionError('Tried to `unwrap` a `None`')
+  }
+  
+  unwrapOr<T>(def: T): T {
+    return def
+  }
+
+  unwrapOrElse<T, Fn extends () => T>(fn: Fn): T {
+    return fn()
+  }
+
+  map<T, U, Fn extends (t: T) => U>(fn: Fn): Option<U> {
+    return this
+  }
+
+  mapOr<T, U, Fn extends (t: T) => U>(def: U, fn: Fn): U {
+    return def
+  }
+
+  mapOrElse<T, U, D extends () => U, Fn extends (t: T) => U>(def: D, fn: Fn): U {
+    return def()
+  }
+
+  okOr<E>(err: E): Result<any, E> {
+    return new Err(err)
+  }
+
+  okOrElse<E, Fn extends () => E>(fn: Fn): Result<any, E> {
+    return new Err(fn())
+  }
+
+  and<U>(andU: U): Option<U> {
+    return this
+  }
+
+  andThen<T, U, Fn extends (t: T) => Option<U>>(fn: Fn): Option<U> {
+    return this
+  }
+
+  or<T>(t: Option<T>): Option<T> {
+    return t
+  }
+
+  orElse<T, Fn extends () => Option<T>>(fn: Fn): Option<T> {
+    return fn()
+  }
+
+  take<T>(): Option<T> {
+    return this
+  }
 }
